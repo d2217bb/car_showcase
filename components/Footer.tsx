a752942afa1f46ac6b1348ba89da2d596ad90aa0
d2217bb <daniela.boros@isa.utm.md>
2025-09-04 20:40:40 +0300

    modified footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { footerLinks } from '@/constants';
 
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-        {/* Logo + descriere */}
+      
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src="/logo.svg"
@@ -22,18 +23,28 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Footer Links */}
+        
         <div className="footer__links flex flex-wrap gap-10">
           {footerLinks.map((link) => (
             <div key={link.title} className="footer__link flex flex-col gap-3">
               <h3 className="font-bold">{link.title}</h3>
               {link.links.map((item) => (
-                <link key={item.title} href={item.url} className="text-gray-500 hover:text-gray-900">
+                <Link key={item.title} href={item.url} className="text-gray-500 hover:text-gray-900">
                   {item.title}
-                </link>
+                </Link>
               ))}
             </div>
           ))}
+          <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+            <div className="footer_copyrights-link">
+              <Link href="/" className='text-gray-500'>
+              Privacy Policy
+              </Link>
+              <Link href="/" className='text-gray-500'>
+              Terms of Use
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
