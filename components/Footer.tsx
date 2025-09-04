3cfa5494428d6eb476318e91faaaf95b66dfb897
d2217bb <daniela.boros@isa.utm.md>
2025-09-04 21:15:20 +0300

    modified footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,6 @@ const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-      
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src="/logo.svg"
@@ -23,28 +22,33 @@ const Footer = () => {
           </p>
         </div>
 
-        
         <div className="footer__links flex flex-wrap gap-10">
           {footerLinks.map((link) => (
             <div key={link.title} className="footer__link flex flex-col gap-3">
               <h3 className="font-bold">{link.title}</h3>
               {link.links.map((item) => (
-                <Link key={item.title} href={item.url} className="text-gray-500 hover:text-gray-900">
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500 hover:text-gray-900"
+                >
                   {item.title}
                 </Link>
               ))}
             </div>
           ))}
-          <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-            <div className="footer_copyrights-link">
-              <Link href="/" className='text-gray-500'>
-              Privacy Policy
-              </Link>
-              <Link href="/" className='text-gray-500'>
-              Terms of Use
-              </Link>
-            </div>
-          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2025 CarHub. All Rights Reserved</p>
+        <div className="flex gap-5">
+          <Link href="/" className="text-gray-500 hover:text-gray-900">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500 hover:text-gray-900">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
