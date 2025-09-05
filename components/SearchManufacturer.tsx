50225871624c945709f3a89cb32832e50c4c9d5e
d2217bb <daniela.boros@isa.utm.md>
2025-09-05 10:05:16 +0300

    modified SearchManufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -58,8 +58,26 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                   }`
                 }
               >
-                
-                {item}
+            
+             {({ selected, active }) => (
+                <>
+                    <span
+                    className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                    }`}
+                    >
+                    {item}
+                    </span>
+                    {selected ? (
+                    <span
+                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                        active ? 'text-white' : 'text-teal-600'
+                        }`}
+                    >
+                    </span>
+                    ) : null}
+                </>
+                )}
               </Combobox.Option>
             ))}
           </Combobox.Options>
